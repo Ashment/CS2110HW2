8bf729f9847ec1d50aa1cebf95f36f5f924fa578
Ashment <gundamme@gmail.com>
2016-07-11 10:41:48 -0400

    WorldMarket Change

--- src/hw2/WorldMarket.java
+++ src/hw2/WorldMarket.java
@@ -1,10 +1,19 @@
 package hw2;
+import hw2.resources.*;
 
 public class WorldMarket {
-
+	
+	Resource worldBiofuel, worldAlloy, worldStim;
+	
+	public WorldMarket(){
+		worldBiofuel = new Biofuel();
+		worldAlloy = new Alloy();
+		worldStim = new Stim();
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }

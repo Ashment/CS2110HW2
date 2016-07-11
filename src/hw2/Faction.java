4c1d72d50265c849c4367c3c649753e43440249f
LockonS483 <rumblie.gg@gmail.com>
2016-07-11 10:58:43 -0400

    Added world and faction initialization

--- src/hw2/Faction.java
+++ src/hw2/Faction.java
@@ -10,36 +10,47 @@ public class Faction {
 	
 	Resource biofuel, alloy, stim;
 	
-	public int biofuelProductionPower, alloyProductionPower, stimProductionPower;
+	public int biofuelProductionPower, alloyProductionPower, stimProductionPower, specialProductionPower;
 	
 	public Faction(String n, int production, String specialR) {
 		name = n;
+		//create faction-specific resource data when faction is created
 		biofuel = new Biofuel();
 		alloy = new Alloy();
 		stim = new Stim();
 		specialty = specialR;
 		
+		//Produce only its special resource
 		switch (specialR){
 			case "Alloy":
 				alloy.SetDemand(0);
 				alloyProductionPower = production;
+				specialProductionPower = alloyProductionPower;
 				biofuelProductionPower = 0;
 				stimProductionPower = 0;
 				break;
 			case "Biofuel":
 				biofuel.SetDemand(0);
 				biofuelProductionPower = production;
+				specialProductionPower = biofuelProductionPower;
 				stimProductionPower = 0;
 				alloyProductionPower = 0;
 				break;
 			case "Stim":
 				stim.SetDemand(0);
 				stimProductionPower = production;
+				specialProductionPower = stimProductionPower;
 				alloyProductionPower = 0;
 				biofuelProductionPower = 0;
 				break;
 			default:
 				break;
 		}
+	}
+	
+	public String toString(){
+		String s = "Factions: " + name + "\n"
+				+ "Producing " + specialty + " at: " + specialProductionPower + "/cycle \n";
+		return s;
 	}
 }
--- src/hw2/Main.java
+++ src/hw2/Main.java
@@ -1,5 +1,21 @@
 package hw2;
 
 public class Main {
-
+	static Main main;
+	public static Faction protoss, zerg, terran;
+	
+	public static void main(String[] args){
+		main = new Main();
+	}
+	public Main(){
+		//Initialize Factions
+		protoss = new Faction("Protoss", 100, "Alloy");
+		zerg = new Faction("Zerg", 80, "Biofuel");
+		terran = new Faction("Terran", 50, "Stim");
+		System.out.println(protoss.toString());
+		System.out.println(zerg.toString());
+		System.out.println(terran.toString());
+		
+		//Initialize World Market
+	}
 }

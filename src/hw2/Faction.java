717775835181f627477c07370d05bbd65b8a7c91
LockonS483 <rumblie.gg@gmail.com>
2016-07-11 19:40:12 -0400

    step 1 of cycle and stuff
    Changed some stuff in Faction and Main to facilitate trade

--- src/hw2/Faction.java
+++ src/hw2/Faction.java
@@ -11,10 +11,11 @@ public class Faction implements Trader{
 	
 	Resource biofuel, alloy, stim, special;
 	
-	public int biofuelProductionPower, alloyProductionPower, stimProductionPower, specialProductionPower;
-	public int cBiofuelDemand, cAlloyDemand, cStimDemand;
+	public int biofuelProductionPower, alloyProductionPower, stimProductionPower, specialProductionPower, productionDeviation;
+	//public int cBiofuelDemand, cAlloyDemand, cStimDemand;
 	
-	public Faction(String n, int production, String specialR, Main m) {
+	public Faction(String n, int production, int productDeviation, String specialR, Main m) {
+		capital = 2500;
 		name = n;
 		//create faction-specific resource data when faction is created
 		biofuel = new Biofuel();
@@ -22,7 +23,8 @@ public class Faction implements Trader{
 		stim = new Stim();
 		specialty = specialR;
 		
-		//Produce only its special resource
+		productionDeviation = productDeviation;
+		//Factions produce only its special resource
 		switch (specialR){
 			case "Alloy":
 				alloy.SetDemand(0);
@@ -61,6 +63,8 @@ public class Faction implements Trader{
 	
 	public int getSupplyOf(String r){
 		int x = 0;
+		
+		//get supply depending on special resource (this method may or may not be actually useless)
 		switch(r){
 			case "Alloy":
 				x = alloy.GetSupply();
@@ -77,5 +81,50 @@ public class Faction implements Trader{
 		}
 		
 		return x;
+	}
+	
+	public void ProduceResources(){
+		
+	}
+	
+	public void DetermineCycleConsumption(){
+		//Determine the amount of each resources to consume in this cycle
+		int i = 0; //desired consumption of resource
+		
+		//Alloy consumption/demand
+		if(special != alloy){
+			//set cAlloyDemand
+			i = main.NumberRandom(27, 39); //max consumption 117
+			alloy.SetDemand(i);
+		}else{
+			int supply = special.GetSupply();
+			i = main.NumberRandom(27, 39); //max consumption 117
+			supply -= i;
+			special.SetSupply(supply);
+		}
+		
+		//Biofuel consumption/demand
+		if(special != biofuel){
+			//set cBiofuelDemand
+			i = main.NumberRandom(21, 31); //max consumption 93
+			biofuel.SetDemand(i);
+		}else{
+			int supply = special.GetSupply();
+			i = main.NumberRandom(21, 31); //max consumption 93
+			supply -= i;
+			special.SetSupply(supply);
+		}
+		
+		//stim consumption/demand
+		if(special != stim){
+			//set cStimDemand
+			i = main.NumberRandom(16, 25); //max consumption 75
+			stim.SetDemand(i);
+		}else{
+			int supply = special.GetSupply();
+			i = main.NumberRandom(16, 25); //max consumption 75
+			supply -= i;
+			special.SetSupply(supply);
+		}
 	}
 }
--- src/hw2/Main.java
+++ src/hw2/Main.java
@@ -3,15 +3,22 @@ package hw2;
 public class Main {
 	static Main main;
 	public static Faction protoss, zerg, terran;
+	Faction[] factions;
 	
 	public static void main(String[] args){
 		main = new Main();
 	}
 	public Main(){
-		//Initialize Factions
-		protoss = new Faction("Protoss", 100, "Alloy", this);
-		zerg = new Faction("Zerg", 80, "Biofuel", this);
-		terran = new Faction("Terran", 50, "Stim", this);
+		//Initialize Factions and set references
+		protoss = new Faction("Protoss", 100, 8, "Alloy", this);
+		zerg = new Faction("Zerg", 80, 12, "Biofuel", this);
+		terran = new Faction("Terran", 60, 6, "Stim", this);
+		factions = new Faction[3];
+		factions[0] = protoss;
+		factions[1] = zerg;
+		factions[2] = terran;
+		
+		//debug print for faction initialization
 		System.out.println(protoss.toString());
 		System.out.println(zerg.toString());
 		System.out.println(terran.toString());

c85d9b4d5edc3d12183b9cbef9bc654107e5f940
Ashment <gundamme@gmail.com>
2016-07-11 10:27:56 -0400

    Added Faction and WorldMarket

---
+++ src/hw2/Faction.java
@@ -0,0 +1,15 @@
+package hw2;
+
+public class Faction {
+
+	public int capital;
+	
+	Resource biofuel, alloy, stim;
+	
+	public int biofuelProductionPower, alloyProductionPower, stimProductionPower;
+	
+	public Faction() {
+		
+	}
+	
+}

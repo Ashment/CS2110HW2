b76db8f6c5e98e0c55066d95bbcac594384dd396
LockonS483 <rumblie.gg@gmail.com>
2016-07-11 10:41:30 -0400

    Added constructor for Faction

--- src/hw2/Faction.java
+++ src/hw2/Faction.java
@@ -5,13 +5,41 @@ import hw2.resources.*;
 public class Faction {
 
 	public int capital;
+	public String name;
+	public String specialty;
 	
 	Resource biofuel, alloy, stim;
 	
 	public int biofuelProductionPower, alloyProductionPower, stimProductionPower;
 	
-	public Faction() {
+	public Faction(String n, int production, String specialR) {
+		name = n;
+		biofuel = new Biofuel();
+		alloy = new Alloy();
+		stim = new Stim();
+		specialty = specialR;
 		
+		switch (specialR){
+			case "Alloy":
+				alloy.SetDemand(0);
+				alloyProductionPower = production;
+				biofuelProductionPower = 0;
+				stimProductionPower = 0;
+				break;
+			case "Biofuel":
+				biofuel.SetDemand(0);
+				biofuelProductionPower = production;
+				stimProductionPower = 0;
+				alloyProductionPower = 0;
+				break;
+			case "Stim":
+				stim.SetDemand(0);
+				stimProductionPower = production;
+				alloyProductionPower = 0;
+				biofuelProductionPower = 0;
+				break;
+			default:
+				break;
+		}
 	}
-	
 }
--- src/hw2/resources/Resource.java
+++ src/hw2/resources/Resource.java
@@ -3,6 +3,13 @@ package hw2.resources;
 public class Resource {
 	int supply, demand;
 	static int worldSupply, worldDemand;
-	int value;
-	public String name;
+	private static int maxValue, minValue;
+	public static String name;
+	
+	public void SetDemand(int d){
+		demand = d;
+	}
+	public void SetSupply(int s){
+		supply = s;
+	}
 }

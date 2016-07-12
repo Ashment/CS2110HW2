e8a514ddd08486204f00bcd7802d0865242483c5
Ashment <gundamme@gmail.com>
2016-07-11 21:33:10 -0400

    Market Cycle Draft

--- src/hw2/WorldMarket.java
+++ src/hw2/WorldMarket.java
@@ -4,9 +4,13 @@ import hw2.resources.*;
 public class WorldMarket {
 	
 	Resource worldBiofuel, worldAlloy, worldStim;
-	int basePrcB = 120, basePrcA = 100, basePrcS = 200;
+	int basePrcB = 60, basePrcA = 50, basePrcS = 100;
+	Trader PTrader, ZTrader, TTrader;
+	Main main;
 	
-	public WorldMarket(){
+	public WorldMarket(Main m){
+		main = m;
+		
 		worldBiofuel = new Biofuel();
 		worldAlloy = new Alloy();
 		worldStim = new Stim();
@@ -14,6 +18,57 @@ public class WorldMarket {
 		worldBiofuel.setPrice(basePrcB);
 		worldAlloy.setPrice(basePrcA);
 		worldStim.setPrice(basePrcS);
+		
+		PTrader = main.terran;
+		ZTrader = main.zerg;
+		TTrader = main.terran;
+	}
+	
+	public void NewCycle(){
+		//Initialize Cycle
+		main.zerg.ProduceResources();
+		main.zerg.DetermineCycleConsumption();
+		main.protoss.ProduceResources();
+		main.protoss.DetermineCycleConsumption();
+		main.terran.ProduceResources();
+		main.terran.DetermineCycleConsumption();
+		
+		//Transactions
+		ResourceTransaction(PTrader, ZTrader, "Biofuel");
+		ResourceTransaction(PTrader, TTrader, "Stim");
+		ResourceTransaction(ZTrader, PTrader, "Alloy");
+		ResourceTransaction(ZTrader, TTrader, "Stim");
+		ResourceTransaction(TTrader, ZTrader, "Biofuel");
+		ResourceTransaction(TTrader, PTrader, "Alloy");
+		System.out.println("Transactions Complete.");
+		
+		//Finalize
+	}
+	
+	public void ResourceTransaction(Trader buyer, Trader seller, String tradedResource){
+		int buyerDemand;
+		
+		if(buyer.getDemandOf(tradedResource) > seller.getSupplyOf(tradedResource)){
+			buyer.setDemand(tradedResource, seller.getSupplyOf(tradedResource));
+		}
+		
+		//Resource Transaction
+		seller.changeSupplyOf(tradedResource, buyer.getDemandOf(tradedResource));
+		//Buyer Consumes Resource!
+		
+		//Capital Transaction
+		if(tradedResource == "Biofuel"){
+			seller.changeCapital(worldBiofuel.getPrice() * buyer.getDemandOf(tradedResource));
+			buyer.changeCapital(-worldBiofuel.getPrice() * buyer.getDemandOf(tradedResource));
+		}else if(tradedResource == "Alloy"){
+			seller.changeCapital(worldAlloy.getPrice() * buyer.getDemandOf(tradedResource));
+			buyer.changeCapital(-worldAlloy.getPrice() * buyer.getDemandOf(tradedResource));
+		}else if(tradedResource == "Stim"){
+			seller.changeCapital(worldStim.getPrice() * buyer.getDemandOf(tradedResource));
+			buyer.changeCapital(-worldStim.getPrice() * buyer.getDemandOf(tradedResource));
+		}else{
+			System.out.println("Invalid traded resource");
+		}
 	}
 	
 	public static void main(String[] args) {
